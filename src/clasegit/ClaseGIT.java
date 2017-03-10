89ab49f7a2bdaa367cb66953c3e547125e6245ee
RobertoDAM <robertogelzsandamian12@gmail.com>
2017-03-10 14:20:12 +0100

    Añadida linea 2

--- src/clasegit/ClaseGIT.java
+++ src/clasegit/ClaseGIT.java
@@ -6,6 +6,7 @@ public class ClaseGIT {
     
     public static void main(String[] args) {
         System.out.println("Inicio de proyecto");
+        System.out.println("Inicio de proyecto");
     }
     
 }
